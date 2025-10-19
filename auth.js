9603849540bc8fa3a3e6bc3392a52696e612b1df
robloxpresidentoffice-bit <roblox.president.office@gmail.com>
2025-10-20 02:13:28 +0900

    auth.js
    `<@${userId}>님, **${existing.robloxName}** 계정으로 연동이 완료되었습니다.\n\n\u200B`
    ------------------------------
    공백문자 추가

--- auth.js
+++ auth.js
@@ -146,7 +146,7 @@ export async function setupAuth(client) {
         const embed = new EmbedBuilder()
           .setTitle("인증이 완료되었습니다.")
           .setDescription(
-            `<@${userId}>님, **${existing.robloxName}** 계정으로 연동이 완료되었습니다.\n\n`
+            `<@${userId}>님, **${existing.robloxName}** 계정으로 연동이 완료되었습니다.\n\n\u200B`
           )
           .setColor(0x00a67e)
           .setFooter({
@@ -315,3 +315,4 @@ export async function setupAuth(client) {
     }
   });
 }
+
